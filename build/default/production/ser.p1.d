a49e4602b358a87ba9e89d0f3baea5f3947de002
John Rahme <john.rahme.se@gmail.com>
2016-04-23 14:01:47 +1100

    still trying the git ignore

--- build/default/production/ser.p1.d
+++ build/default/production/ser.p1.d
@@ -1,5 +1,1 @@
- build/default/production/ser.d  \
- build/default/production/ser.p1:  \
- ser.c  \
-common.h  \
-ser.h 
+ build/default/production/ser.d  build/default/production/ser.p1:  ser.c common.h ser.h
